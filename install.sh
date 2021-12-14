0df0b33f00ce2d15ceeb2fa20d80c4617f3590c7
Rocktim Saikia <rocktimthedev@gmail.com>
2021-12-15 00:17:42 +0530

    set up plugins
    add Pure as default prompt
    
    update install script with new changes

--- install.sh
+++ install.sh
@@ -9,3 +9,12 @@ mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh"
+
+# set up external dependencies
+# check and install Pure, a minimal prompt
+PROMPT_PURE_DIR="$HOME/.dotfiles/zsh/plugins/pure"
+if [ ! -d "$PROMPT_PURE_DIR" ]; then
+    git clone https://github.com/sindresorhus/pure.git "$PROMPT_PURE_DIR"
+fi
+
+ln -sf "$HOME/.dotfiles/zsh/plugins" "$HOME/.config/zsh"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,9 @@
 source "$XDG_CONFIG_HOME/zsh/aliases"
+
+# load everything external dependencies
+fpath+="$XDG_CONFIG_HOME/zsh/plugins/pure"
+
+# init prompt and pick pure
+autoload -U promptinit; promptinit
+zstyle :prompt:pure:git:stash show yes
+prompt pure
