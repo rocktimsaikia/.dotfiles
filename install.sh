2c4684324c1909e446f8254c26a6f296583ac188
Rocktim Saikia <rocktimthedev@gmail.com>
2021-12-15 11:16:59 +0530

    remove redundant git clone script, use submodules

--- install.sh
+++ install.sh
@@ -10,11 +10,5 @@ ln -sf "$HOME/.dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh"
 
-# set up external dependencies
-# check and install Pure, a minimal prompt
-PROMPT_PURE_DIR="$HOME/.dotfiles/zsh/plugins/pure"
-if [ ! -d "$PROMPT_PURE_DIR" ]; then
-    git clone https://github.com/sindresorhus/pure.git "$PROMPT_PURE_DIR"
-fi
-
+# symlink the external submodules
 ln -sf "$HOME/.dotfiles/zsh/plugins" "$HOME/.config/zsh"
