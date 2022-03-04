b93c14c709ab4d8e3e14ec456630dab5f804693b
Rocktim Saikia <rocktimthedev@gmail.com>
2022-03-04 13:51:44 +0530

    refactor

--- theme.sh
+++ theme.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# This is only if you use Ubuntu gnome terminal
-# Checks if gogh is already added. If not set it up and install a theme
-# Sets tokyonight theme by default
+# Color Scheme for Gnome Terminal
+# theme: tokyonight
 if [ ! -d "$HOME/src/gogh" ]; then
     mkdir -p "$HOME/src"
     git clone https://github.com/Mayccoll/Gogh.git "$HOME/src/gogh"
