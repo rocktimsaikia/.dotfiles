beb8daeb88ae3384dd99dd7054d5a139dbceee71
Rocktim Saikia <rocktimthedev@gmail.com>
2022-03-18 21:57:59 +0530

    add tmux conf

--- install.sh
+++ install.sh
@@ -13,3 +13,6 @@ ln -sf "$HOME/.dotfiles/zsh" "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/aliases" "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/antigen.zsh" "$HOME/.config/zsh"
 ln -sf "$HOME/.dotfiles/zsh/pure" "$HOME/.config/zsh"
+
+# symlink tmux conf file
+ln -sf "$HOME/.dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
